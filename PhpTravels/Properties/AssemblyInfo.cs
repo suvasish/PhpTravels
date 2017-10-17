7f979484ee41910555280b8ab58b930b8563b01c
suvasish <sarkers@hsntech.com>
2017-10-17 17:15:26 +1100

    Add project files.

---
+++ PhpTravels/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PhpTravels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hansen Technologies")]
+[assembly: AssemblyProduct("PhpTravels")]
+[assembly: AssemblyCopyright("Copyright © Hansen Technologies 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b272089-e5cb-4ef8-b406-3f693fca1c97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PhpTravels/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace PhpTravels
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void Finalize()
+        {
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+    }
+}
