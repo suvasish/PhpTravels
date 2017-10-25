656e547d4554699fe3ae5504a5ebb34df9ac03b2
Suvasish Sarker <sarkers@hsntech.com>
2017-10-25 17:26:14 +1100

    Adding config file

--- PhpTravels/TestCases/SampleTest.cs
+++ PhpTravels/TestCases/SampleTest.cs
@@ -1,36 +1,14 @@
 ﻿using System;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
-namespace PhpTravels
+namespace PhpTravels.TestCases
 {
-    public class SampleTest
+    public class SampleTest: TestBase
     {
-        IWebDriver driver;
-
-        [SetUp]
-        public void Initialize()
-        {
-            driver = new ChromeDriver();
-        }
-
         [Test]
         public void TC_open_travel_agency_site()
         {
-            driver.Navigate().GoToUrl("http://www.phptravels.net/");
-            driver.Manage().Window.Maximize();
-
-            // Make sure the page loaded successfully.
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until<IWebElement>(d => d.FindElement(By.ClassName("logo")));
-        }
-
-        [TearDown]
-        public void CleanUp()
-        {
-            driver.Quit();
+            OpenUrl();
         }
     }
 }
---
+++ PhpTravels/TestCases/TestBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace PhpTravels.TestCases
+{
+    [TestFixture]
+    public class TestBase
+    {
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+        public void OpenUrl()
+        {
+            driver.Navigate().GoToUrl("http://www.phptravels.net/");
+            driver.Manage().Window.Maximize();
+
+            // Make sure the page loaded successfully.
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until<IWebElement>(d => d.FindElement(By.ClassName("logo")));
+        }
+    }
+}
