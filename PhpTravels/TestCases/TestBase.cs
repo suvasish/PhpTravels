323311c5a54aac0adb7790f4bee5edc67c26bcb8
Suvasish Sarker <sarkers@hsntech.com>
2017-10-26 18:00:28 +1100

    Updatted TestBase class to handle App.config

--- PhpTravels/TestCases/TestBase.cs
+++ PhpTravels/TestCases/TestBase.cs
@@ -12,11 +12,13 @@ namespace PhpTravels.TestCases
     {
         IWebDriver driver;
 
-
         [SetUp]
         public void Initialize()
         {
-            driver = new ChromeDriver();
+            if (GetBrowser().ToLower() == "chrome")
+            {
+                driver = new ChromeDriver();
+            }
         }
 
         [TearDown]
@@ -27,12 +29,24 @@ namespace PhpTravels.TestCases
 
         public void OpenUrl()
         {
-            driver.Navigate().GoToUrl("http://www.phptravels.net/");
+            driver.Navigate().GoToUrl(GetUrl());
             driver.Manage().Window.Maximize();
 
             // Make sure the page loaded successfully.
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             wait.Until<IWebElement>(d => d.FindElement(By.ClassName("logo")));
+        }
+
+        private string GetUrl()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            return appSettings["Url"];
+        }
+
+        private string GetBrowser()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            return appSettings["Browser"];
         }
     }
 }
